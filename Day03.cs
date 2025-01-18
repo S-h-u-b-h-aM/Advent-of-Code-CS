c21e99b21623d587e72c759f4cd2276b8eb23ac6
S-h-u-b-h-aM <shubhamprataprdx@gmail.com>
2025-01-19 00:37:06 +0530

    Added description in Day 03 class

--- Day03.cs
+++ Day03.cs
@@ -4,6 +4,23 @@ namespace Advent_of_Code;
 
 static class Day03
 {
+    public static string Description
+    {
+        get => "--- Day 3: Mull It Over ---\n\"" +
+               "Our computers are having issues, so I have no idea if we have any Chief Historians in stock! You're welcome to check the warehouse, " +
+               "though,\" says the mildly flustered shopkeeper at the North Pole Toboggan Rental Shop. The Historians head out to take a look.\n" +
+               "\nThe shopkeeper turns to you. \"Any chance you can see why our computers are having issues again?\"\n" +
+               "\nThe computer appears to be trying to run a program, but its memory (your puzzle input) is corrupted. " +
+               "All of the instructions have been jumbled up!\n\nIt seems like the goal of the program is just to multiply some numbers. " +
+               "It does that with instructions like mul(X,Y), where X and Y are each 1-3 digit numbers. " +
+               "For instance, mul(44,46) multiplies 44 by 46 to get a result of 2024. Similarly, mul(123,4) would multiply 123 by 4.\n" +
+               "\nHowever, because the program's memory has been corrupted, there are also many invalid characters that should be ignored, even if they look like part of a mul instruction. " +
+               "Sequences like mul(4*, mul(6,9!, ?(12,34), or mul ( 2 , 4 ) do nothing.\n" +
+               "\nFor example, consider the following section of corrupted memory:\n" +
+               "\nxmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))\nOnly the four highlighted sections are real mul instructions. " +
+               "Adding up the result of each instruction produces 161 (2*4 + 5*5 + 11*8 + 8*5).\n\nScan the corrupted memory for uncorrupted mul instructions. " +
+               "What do you get if you add up all of the results of the multiplications?"; 
+    }
     public static void Run()
     {
         var text = Console.In.ReadLines();
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ public static class Program
         // Console.WriteLine("3  4\n4  3\n2  5\n1  3\n3  9\n3  3");
         // Day01.Execute();
         Console.WriteLine("mul(2,4)%mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul()8,5)");
+        
+        Console.WriteLine(Day03.Description);
         Day03.Run();
     }
 }
