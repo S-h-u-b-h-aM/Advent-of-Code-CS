5c90eeec0bc2187df72919e4f2ea772dd40722c4
S-h-u-b-h-aM <shubhamprataprdx@gmail.com>
2025-01-18 22:13:55 +0530

    Day 02 completed.

---
+++ Day02.cs
@@ -0,0 +1,46 @@
+using System.Dynamic;
+
+namespace Advent_of_Code;
+
+static class Day02
+{
+    public static void Run()
+    {
+        List<List<int>> allLists = Console.In.LoadLists();
+        int safeCount = allLists.Count(IsSafe);
+        int tolerantSafeCount = allLists.Count(list => list.Expand().Any(IsSafe));
+        
+        Console.WriteLine($"Total count: {allLists.Count}");
+        Console.WriteLine($"Total safe count: {safeCount}");
+        Console.WriteLine($"Total tolerant safe count: {tolerantSafeCount}");
+    }
+
+    private static IEnumerable<List<int>> Expand(this List<int> values) =>
+        new[] { values }.Concat(
+            Enumerable.Range(0, values.Count).Select(values.ExceptAt));
+    private static List<int> ExceptAt(this List<int> values, int index) =>
+        values.Take(index).Concat(values.Skip(index + 1)).ToList();
+    private static bool IsSafe(this List<int> values) =>
+        values.Count < 2 || values.IsSafe(Math.Sign(values[1] - values[0]));
+    private static bool IsSafe(this List<int> values, int diffSign) =>
+        values.Pairs().All(pair => pair.IsSafe(diffSign));
+
+    private static IEnumerable<(int prev, int next)> Pairs(this IEnumerable<int> values)
+    {
+        using var enumerator = values.GetEnumerator();
+        if (!enumerator.MoveNext()) yield break;
+        
+        int prev = enumerator.Current;
+        while (enumerator.MoveNext())
+        {
+            yield return (prev, enumerator.Current);
+            prev = enumerator.Current;
+        }
+    }
+    private static bool IsSafe(this (int prev, int next) pair, int diffSign) =>
+        Math.Abs(pair.next - pair.prev) >= 1 &&
+        Math.Abs(pair.next - pair.prev) <= 3 &&
+        Math.Sign(pair.next - pair.prev) == diffSign;
+    private static List<List<int>> LoadLists(this TextReader text) => 
+        text.ReadLines().Select(Common.ParseIntsNoSign).ToList();
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using Advent_of_Code;
 
-static class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
         // Console.WriteLine("3  4\n4  3\n2  5\n1  3\n3  9\n3  3");
-        Day01.Execute();
+        // Day01.Execute();
+        Day02.Run();
     }
 }
