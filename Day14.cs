11711fc7a42ceed1ce5117d4cc22786b6469f81d
S-h-u-b-h-aM <shubhamprataprdx@gmail.com>
2025-05-31 18:14:00 +0530

    Day 14 Part 2 added.

--- Day14.cs
+++ Day14.cs
@@ -10,9 +10,60 @@ public static class Day14
         
         int safetyFactor = robots.Move(time, roomSize).GetSafetyFactor(roomSize);
         Console.WriteLine($"Safety factor: {safetyFactor}");
-        
+
+        foreach ((List<Robot> state, int time) candidate in robots.GetXMasTreeCoordinates(roomSize))
+        {
+            candidate.state.Print(candidate.time, roomSize);
+            Console.Write($"Keep searching? (y/n) ");
+            if (Console.ReadLine()?.ToLower() == "n") break;
+        }
     }
 
+    private static IEnumerable<(List<Robot> state, int time)> GetXMasTreeCoordinates(
+        this IEnumerable<Robot> robots, Coordinates roomSize) =>
+        Enumerable.Range(0, int.MaxValue)
+            .Where(time => robots.Move(time, roomSize).MaybeXMaxTree())
+            .Select(time => (robots.Move(time, roomSize).ToList(), time));
+    private static bool MaybeXMaxTree(this IEnumerable<Robot> robots) =>
+        robots.GetGroupSizes().Max() >= robots.Count() / 3;
+    private static IEnumerable<int> GetGroupSizes(this IEnumerable<Robot> robots)
+    {
+         HashSet<Coordinates> pending = robots.Select(robot => robot.Position).ToHashSet();
+         while (pending.Count > 0)
+         {
+             Coordinates pivot = pending.First();
+             pending.Remove(pivot);
+
+             var add = new Queue<Coordinates>();
+             add.Enqueue(pivot);
+             
+             int groupSize = 0;
+             while (add.TryDequeue(out var current))
+             {
+                 groupSize++;
+
+                 Coordinates[] neighbours = new[]
+                 {
+                     current with { X = current.X - 1 },
+                     current with { X = current.X + 1 },
+                     current with { Y = current.Y - 1 },
+                     current with { Y = current.Y + 1 },
+                 };
+                 foreach (Coordinates next in neighbours.Where(pending.Remove)) add.Enqueue(next);
+             }
+             yield return groupSize;
+         }
+    }
+    private static void Print(this IEnumerable<Robot> robots, int time, Coordinates roomSize)
+    {
+        char[][] map = Enumerable.Range(0, roomSize.Y)
+            .Select(_ => Enumerable.Repeat('.', roomSize.X).ToArray()).ToArray();
+        robots.ToList().ForEach(robot => map[robot.Position.Y][robot.Position.X] = '#');
+        
+        Console.WriteLine();
+        Console.WriteLine($"Time: {time}");
+        map.Select(row => new string(row)).ToList().ForEach(Console.WriteLine);
+    }
     private static int GetSafetyFactor(this IEnumerable<Robot> robots, Coordinates roomSize) =>
         robots.ToQuadrantCounts(roomSize).Aggregate(1, (safety, count) => safety * count);
     private static IEnumerable<int> ToQuadrantCounts(this IEnumerable<Robot> robots, Coordinates roomSize) => new[]
